e6f565724aec33ec9338f221028e1e6f35990037
quentina00 <quentinartaud@gmail.com>
2024-09-13 19:46:30 +0200

    changed logo, changed behaviour for settings in menuBar, created new mixin for links + changed style for links

--- src/components/header/menuBar/MenuBarElement.tsx
+++ src/components/header/menuBar/MenuBarElement.tsx
@@ -4,6 +4,7 @@ import MenuBarSettings from "./MenuBarSettings"
 import { useLocation } from "react-router-dom"
 import AnimationWrapper from "../../AnimationWrapper"
 import { zoomEffect } from "../../../style/animations/animations"
+import ButtonWithIcon from "../../ButtonWithIcon"
 
 interface MenuBarElementProps {
     menuBarElementData:MenuBarElementInterface
@@ -18,9 +19,13 @@ const MenuBarElement:React.FC<MenuBarElementProps> = ({ menuBarElementData }) =>
     // slice is used because pathname is returning a path with a "/", so we want to gather only the path name without the "/"
     const isElementSelected = location.pathname.slice(1) === menuBarElementData.id
 
+    // to check if it's the settings element
+    const isSettingsElement = menuBarElementData.id === 'settings'
+
     return (
         <div className="menuBarElement">
-            <p>{menuBarElementData.text}</p>
+            {!isSettingsElement && <p>{menuBarElementData.text}</p>}
+            {isSettingsElement && <ButtonWithIcon imageName="settings.svg"/>}
 
             <AnimatePresence mode="wait">
                 {isElementSelected &&
