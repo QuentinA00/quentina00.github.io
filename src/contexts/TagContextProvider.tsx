40857564ffee765340168207f6c4210d2223cbf1
quentinvespero <quentinvespero@gmail.com>
2025-09-28 21:41:34 +0200

    add a function in the context, to get multiple tag objects at once, instead of only one previously

--- src/contexts/TagContextProvider.tsx
+++ src/contexts/TagContextProvider.tsx
@@ -5,18 +5,26 @@ import tags from '../../public/assets/jsons/tags.json'
 interface TagsContextType {
     tags: TagInterface[]
     getTagById: (id: string) => TagInterface | undefined
+    getTagsByIds: (ids:string[]) => TagInterface[]
 }
 
 const TagsContext = createContext<TagsContextType | undefined>(undefined)
 
 export const TagsProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
+    // return the tag that has the id given as parameter
+    // if no match, return undefined
     const getTagById = (id:string) => {
         return tags.find(tag => tag.id == id)
     }
 
+    // return the tags objects, from an array of tag id
+    const getTagsByIds = (ids:string[]) => {
+        return ids.map((id => getTagById(id))).filter((tag): tag is TagInterface => Boolean(tag)) // the filter here retain only the tag object (and filter out the case where getTagById return undefined)
+    }
+
     return (
-        <TagsContext.Provider value={{tags,getTagById}}>
+        <TagsContext.Provider value={{tags,getTagById, getTagsByIds}}>
             {children}
         </TagsContext.Provider>
     )
