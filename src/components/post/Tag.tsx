46658c49be31e32c41809630b4dbb5647b7ff7c6
quentinvespero <quentinvespero@gmail.com>
2025-09-29 21:45:58 +0200

    add a new prop to manage when the tag is selected

--- src/components/post/Tag.tsx
+++ src/components/post/Tag.tsx
@@ -3,9 +3,9 @@ import styled from "styled-components"
 import { useTags } from "../../contexts/TagContextProvider"
 
 interface TagProps {
-    // text?:PostTagsInterface['text']
     className?:string
     tagId:string
+    isSelected?:boolean
 }
 
 const TagGlobalStyle = styled.div`
@@ -31,7 +31,7 @@ const TagGlobalStyle = styled.div`
     }
 `
 
-const Tag:React.FC<TagProps> = ({className, tagId}) => {
+const Tag:React.FC<TagProps> = ({className, tagId, isSelected}) => {
 
     const {getTagById} = useTags()
 
@@ -43,7 +43,7 @@ const Tag:React.FC<TagProps> = ({className, tagId}) => {
     }
 
     return (
-        <TagGlobalStyle className={`tag tag-${className}`}>
+        <TagGlobalStyle className={`tag tag-${className} ${isSelected ? 'tag' : 'tag-selected'}`}>
             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle className="circle1" cx="9" cy="9" r="4" fill="#F1F1F1"/>
                 <circle className="circle2" cx="9" cy="9" r="8.15" stroke="#F1F1F1" strokeWidth="1.7"/>
