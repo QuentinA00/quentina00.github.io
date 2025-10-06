af67526fb0c141d3a34462a1fe005833590a9082
quentinvespero <quentinvespero@gmail.com>
2025-10-06 20:07:13 +0200

    changed import to keep only youtube so react-player doesn't generate a bunch of useless files at build

--- src/components/MediaComponent.tsx
+++ src/components/MediaComponent.tsx
@@ -1,4 +1,4 @@
-import ReactPlayer from "react-player"
+import ReactPlayer from "react-player/youtube"
 import { PostInterface, PostMediasInterface } from "../interfaces/postsInterfaces"
 import { screen_tablet } from "../utils/responsiveUtils"
 import { useMediaQuery } from "react-responsive"
