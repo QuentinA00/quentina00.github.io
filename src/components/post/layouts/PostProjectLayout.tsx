a03f13265721cbe2b3f68e49b6e01ae0270f1650
quentinvespero <quentinvespero@gmail.com>
2025-10-22 21:42:15 +0200

    implemented basic lazy loading

--- src/components/post/layouts/PostProjectLayout.tsx
+++ src/components/post/layouts/PostProjectLayout.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, lazy, Suspense, useRef } from "react"
 import { PostInterface } from "../../../interfaces/postsInterfaces"
 import PostLinks from "../elements/PostLinks"
 import PostKeypoints from "../elements/PostKeypoints"
@@ -6,7 +6,7 @@ import PostText from "../elements/PostText"
 import PostTags from "../elements/PostTags"
 import PostTitle from "../elements/PostTitle"
 import PostOrigin from "../elements/PostOrigin"
-import PostMedias from "../elements/PostMedias"
+import { useInView } from "framer-motion"
 
 interface PostProjectLayoutProps {
     postData: PostInterface
@@ -14,7 +14,13 @@ interface PostProjectLayoutProps {
     mobileScreen: boolean
 }
 
+const PostMedias = lazy(() => import('../elements/PostMedias'))
+
 const PostProjectLayout: FC<PostProjectLayoutProps> = ({postData, tabletScreen, mobileScreen}) => {
+
+    const containerRef = useRef<HTMLDivElement | null>(null)
+    const inView = useInView(containerRef, { margin: "300px 0px 300px 0px", amount: 0 })
+
     return (
         <>
             <div className={`postTopSection ${tabletScreen ? 'postTopSection-mobile' : ''}`}>
@@ -22,8 +28,14 @@ const PostProjectLayout: FC<PostProjectLayoutProps> = ({postData, tabletScreen, 
                 <PostTitle title={postData.title} description={postData.description} showDot={!tabletScreen && !!postData.description} className={mobileScreen ? 'postTitle-mobile' : ''} />
                 <PostTags tagsId={postData.tagsId} className='project' />
             </div>
-            <div className={`postContentSection postContentSection-project ${tabletScreen ? 'postContentSection-smallerScreen' : ''}`}>
-                <PostMedias medias={postData.medias}/>
+            <div ref={containerRef} className={`postContentSection postContentSection-project ${tabletScreen ? 'postContentSection-smallerScreen' : ''}`}>
+                
+                <Suspense fallback={null}>
+                    {inView && 
+                        <PostMedias medias={postData.medias}/>
+                    }
+                </Suspense>
+                
                 <div className="postContentSection-description">
                     <PostText textParagraphs={postData.postTextParagraphs}/>
                     <div className="divider3"></div>
